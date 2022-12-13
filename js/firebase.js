e2da2c910e69d2e2e1205d93228c3f75b904c36f
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-12-13 15:15:13 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlMAfeuNRWt4huXru31RHxM8MDobaZMVg",
+    authDomain: "rifa-terceirao.firebaseapp.com",
+    databaseURL: "https://rifa-terceirao-default-rtdb.firebaseio.com",
+    projectId: "rifa-terceirao",
+    storageBucket: "rifa-terceirao.appspot.com",
+    messagingSenderId: "515254292521",
+    appId: "1:515254292521:web:e863ef87cc211492bd4c38",
+    measurementId: "G-060GK5DYE3"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
